46cb546954ae86cd9f191f7b9f58b07311f88ff5
Scott Solomko <scottsolomko@gmail.com>
2012-12-08 08:07:20 -0600

    removed note redundancy

--- js/controllers.js
+++ js/controllers.js
@@ -3,23 +3,18 @@
 function NotesController($scope) {
 
     var savedNotes;
-    var sessionNotes;
 
     // get all the notes from local storage and use them for this session
     savedNotes   = JSON.parse(localStorage.getItem('notes'));
-    if (savedNotes !== null) {
-        sessionNotes = savedNotes;
-    } else {
-        savedNotes   = [];
-        sessionNotes = [];
+    if (savedNotes == null) {
+        savedNotes = [];
     }
 
     // angular monitors this
-    $scope.notes = sessionNotes;
+    $scope.notes = savedNotes;
 
     $scope.addNote = function() {
         var n = {"title" : $scope.noteTitle, "date" : new Date().getTime(), "msg" : $scope.noteMessage};
-        sessionNotes.push(n);
         savedNotes.push(n);
         localStorage.setItem('notes', JSON.stringify(savedNotes));
     }
