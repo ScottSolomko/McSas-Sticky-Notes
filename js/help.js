4b9ae7d10424088b50001fbbfd122a5090a07ef4
Scott Solomko <scottsolomko@gmail.com>
2012-12-13 00:20:28 -0600

    basic accordion function

---
+++ js/help.js
@@ -0,0 +1,10 @@
+$(function() {
+
+    $('article').accordion({
+        collapsible:    true,
+        header:         "h2",
+        heightStyle:    "fill",
+        icons:          false
+    });
+
+});
