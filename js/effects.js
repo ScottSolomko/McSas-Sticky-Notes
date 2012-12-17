28770119615297f75e550ed1b67ec1449cc7ef49
Scott Solomko <scottsolomko@gmail.com>
2012-12-17 02:59:03 -0600

    Help topics are folded by default

--- js/effects.js
+++ js/effects.js
@@ -9,7 +9,11 @@ $(function() {
     $('#help').hide();              // hide the Help on initial load
     $('#aboutWindow').hide();       // hide the About window on initial load
 
+    /**
+     * Help FAQ's accordion style
+     */
     $('#help article').accordion({
+        active:         false,
         collapsible:    true,
         header:         "h2",
         heightStyle:    "content",
