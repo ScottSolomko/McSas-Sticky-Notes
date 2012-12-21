190be63a9562b1df679580ea9c1c0ba50840baff
Scott Solomko <scottsolomko@gmail.com>
2012-12-21 00:16:35 -0600

    complete rewrite of sorting notes

--- js/controllers.js
+++ js/controllers.js
@@ -18,22 +18,21 @@ angular.module('mcsas', []).directive('myPostRepeatDirective', function() {
 });
 
 function NotesController($scope) {
+    var notesContainer;
 
-    var savedNotes = JSON.parse(localStorage.getItem('notes'));
-
-    if (savedNotes == null || savedNotes.length < 1) {
-        savedNotes = [];
-
-        var n = {"title" : "Read Help", "date" : new Date().getTime(), "msg" : "Click on the \"Help\" icon where you will learn how to create and delete notes."};
-        savedNotes.push(n);
-        localStorage.setItem("notes", JSON.stringify(savedNotes));
-    }
-
-    $scope.notes        = savedNotes;
+    $scope.notes        = JSON.parse(localStorage.getItem('notes'));
     $scope.noteMessage  = '';
     $scope.noteTitle    = '';
     $scope.noteId       = 0;
     $scope.submitButton = 'Add';
+
+    if ($scope.notes == null || $scope.notes < 1) {
+        $scope.notes = [];
+
+        var n = {"title" : "Read Help", "date" : new Date().getTime(), "msg" : "Click on the \"Help\" icon where you will learn how to create and delete notes."};
+        $scope.notes.push(n);
+        localStorage.setItem("notes", JSON.stringify($scope.notes));
+    }
 
     $scope.submitNote = function() {
         if ($scope.noteId > 0) {
@@ -45,20 +44,21 @@ function NotesController($scope) {
 
     $scope.addNote = function() {
         var n = {"title" : $scope.noteTitle, "date" : new Date().getTime(), "msg" : $scope.noteMessage};
-        savedNotes.push(n);
-        localStorage.setItem('notes', JSON.stringify(savedNotes));
-
+        $scope.notes.push(n);
+        localStorage.setItem('notes', JSON.stringify($scope.notes));
+        notesContainer.sortable('refresh');
         $scope.noteMessage = '';
         $scope.noteTitle   = '';
-
         this.flashSuccessSave();
     }
 
     $scope.editNote = function() {
-        for(var i = 0; i < savedNotes.length; i++) {
-            if (savedNotes[i].date == $scope.noteId) {
-                savedNotes[i].title = $scope.noteTitle;
-                savedNotes[i].msg   = $scope.noteMessage;
+        var len = $scope.notes.length;
+
+        for(var i = 0; i < len; i++) {
+            if ($scope.notes[i].date == $scope.noteId) {
+                $scope.notes[i].title = $scope.noteTitle;
+                $scope.notes[i].msg   = $scope.noteMessage;
                 break;
             }
         }
@@ -67,56 +67,57 @@ function NotesController($scope) {
         $scope.noteTitle   = '';
         $scope.noteId = 0;
 
-        localStorage.setItem('notes', JSON.stringify(savedNotes));
+        localStorage.setItem('notes', JSON.stringify($scope.notes));
+        notesContainer.sortable('refresh');                             // not sure if this call is required
         this.flashSuccessSave();
     }
 
     $scope.deleteNote = function(id) {
         var index;
+        var len = $scope.notes.length;
 
-        for(var i = 0; i < savedNotes.length; i++) {
-            if (savedNotes[i].date == id) {
+        for(var i = 0; i < len; i++) {              // I think jquery filter() would work here, instead of for loop
+            if ($scope.notes[i].date == id) {
                 index = i;
                 break;
             }
         }
 
-        savedNotes.splice(index, 1);
-        localStorage.setItem('notes', JSON.stringify(savedNotes));
+        $scope.notes.splice(index, 1);
+        localStorage.setItem('notes', JSON.stringify($scope.notes));
     }
 
-    $scope.reindexNotes = function(noteIds) {
-        var tmpNotes    = [];
-        var idsLen      = noteIds.length;
-        var savedLen    = savedNotes.length;
-
-        for (var i = 0; i < idsLen; i++) {
-            for (var j = 0; j < savedLen; j++) {
-                if (savedNotes[j].date == noteIds[i]) {
-                    tmpNotes.push(savedNotes[j]);
-                    break;
-                }
-            }
-        }
-
-        savedNotes = tmpNotes;
-        localStorage.setItem('notes', JSON.stringify(savedNotes));
+    $scope.dragStart = function(e, ui) {
+        ui.item.data('start', ui.item.index());
     }
+
+    $scope.dragEnd = function(e, ui) {
+        var start = ui.item.data('start'),
+            end   = ui.item.index();
+
+        $scope.notes.splice(end, 0, $scope.notes.splice(start, 1)[0]);
+        $scope.$apply();
+
+        localStorage.setItem("notes", JSON.stringify($scope.notes));
+    }
+
+    notesContainer = $('#notesContainer').sortable({
+        start: $scope.dragStart,
+        update: $scope.dragEnd
+    });
 
     /*
-     *  Animations/Interations
+     * a watch for development, comment out prior to production
      */
-    $scope.animateDeleteNote = function(id) {
-        var li = document.getElementById(id);
+    /*
+    $scope.$watch("notes", function(value) {
+        console.log("Notes: " + value.map(function(e){return e.title}).join(','));
+    },true);
+    */
 
-        $(li).effect("transfer", {to: $('#trashBin')}, 750);
-        $('.ui-effects-transfer').css({
-            'background-color':$(li).css('background-color'),
-            'z-index': 999
-        });
-
-        this.deleteNote(id);
-    }
+    /*
+     * Animations & Interactions
+     */
 
     $scope.showAddNoteForm = function() {
         $scope.noteMessage  = '';
@@ -154,6 +155,18 @@ function NotesController($scope) {
         }, 1500);
     }
 
+    $scope.animateDeleteNote = function(id) {
+        var li = document.getElementById(id);
+
+        $(li).effect("transfer", {to: $('#trashBin')}, 750);
+        $('.ui-effects-transfer').css({
+            'background-color':$(li).css('background-color'),
+            'z-index': 999
+        });
+
+        this.deleteNote(id);
+    }
+
     $scope.toggleHelp = function() {
         $('#help').toggle("slide", {direction:"right"});
     }
@@ -179,4 +192,5 @@ function NotesController($scope) {
     $scope.closeAboutDialog = function() {
         $('#aboutWindow').dialog("close");
     }
+
 }
--- js/effects.js
+++ js/effects.js
@@ -16,29 +16,5 @@ $(function() {
         heightStyle:    "content",
         icons:          false
     });
-
-    $('#notesContainer').sortable({
-        containment: "window",
-        cursor: "move",
-        cursorAt: {left: em(7), top: em(1)},
-        forcePlaceholderSize: true,
-        opacity: 0.85,
-        placeholder: "sortablePlaceHolder",
-        revert: true,
-        tolerance: "pointer",
-        update: function(e, ui) {
-            var theIds = $("#notesContainer").sortable("toArray");
-
-            /*
-             * UGLY, UGLY code ahead
-             *
-             * fix this, this is terrible
-             */
-            var ang = angular.element('body').scope();
-            ang.reindexNotes(theIds);
-
-            $('#notesContainer li').css({'z-index':1});
-        }
-    });
-    $('#notesContainer').disableSelection();
+    
 });
